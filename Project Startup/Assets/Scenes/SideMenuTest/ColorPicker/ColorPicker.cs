58235f4508037f76ff85ee082d3753ff431eefae
Emily74x <emilymurad2016@gmail.com>
2023-12-04 23:24:01 +0100

    Fixed color picker to assign color to material
    Code still needs comments!

--- Project Startup/Assets/Scenes/SideMenuTest/ColorPicker/ColorPicker.cs
+++ Project Startup/Assets/Scenes/SideMenuTest/ColorPicker/ColorPicker.cs
@@ -6,36 +6,41 @@ using UnityEngine.EventSystems;
 
 public class ColorPicker : MonoBehaviour, IPointerClickHandler {
 
-  public Color output;
-  private bool canPick = false;
-  [SerializeField] private GameObject ColorWheel;
+  public Material targetMaterial; // Reference to the material you want to change
 
   void Start() {
-
+    // Ensure that a material is assigned to the script
+    if (targetMaterial == null) {
+      Debug.LogError("Please assign a material to the ColorPicker script.");
+    }
   }
 
   void Update() {
-    if (canPick) ColorWheel.SetActive(true);
-    else ColorWheel.SetActive(false);
+  
   }
 
   public void OnPointerClick(PointerEventData eventData) {
-    output = Pick(Camera.main.WorldToScreenPoint(eventData.position), GetComponent<Image>());
+    Color pickedColor = Pick(Camera.main.WorldToScreenPoint(eventData.position), GetComponent<Image>());
+    SetMaterialColor(pickedColor);
   }
 
   Color Pick(Vector2 screenPoint, Image imageToPick) {
     Vector2 point;
     RectTransformUtility.ScreenPointToLocalPointInRectangle(imageToPick.rectTransform, screenPoint, Camera.main, out point);
     point += imageToPick.rectTransform.sizeDelta / 2;
-    Texture2D t = GetComponent<Image>().sprite.texture;
-    Vector2Int m_point = new Vector2Int((int)((t.width * point.x) / imageToPick.rectTransform.sizeDelta.x), (int)((t.height * point.y) / imageToPick.rectTransform.sizeDelta.y)); return t.GetPixel(m_point.x, m_point.y);
+    Texture2D t = imageToPick.sprite.texture;
+    Vector2Int m_point = new Vector2Int((int)((t.width * point.x) / imageToPick.rectTransform.sizeDelta.x), (int)((t.height * point.y) / imageToPick.rectTransform.sizeDelta.y));
+    return t.GetPixel(m_point.x, m_point.y);
   }
 
-  public void ShowPicker() {
-    if (!canPick) canPick = true;
-    else canPick = false;
+  void SetMaterialColor(Color color) {
+    // Check if the material has the "_Color" property
+    if (targetMaterial.HasProperty("_Color")) {
+      // Set the color of the material
+      targetMaterial.SetColor("_Color", color);
+    }
+    else {
+      Debug.LogError("Material does not have a '_Color' property.");
+    }
   }
-
 }
-
-
