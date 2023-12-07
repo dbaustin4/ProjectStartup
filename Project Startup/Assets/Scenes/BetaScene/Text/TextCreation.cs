5e1bdae09437694072d00d46b7681bdabafc244d
Emily74x <emilymurad2016@gmail.com>
2023-12-07 14:39:30 +0100

    Text works!!
    Still have to add and fix details

--- Project Startup/Assets/Scenes/BetaScene/Text/TextCreation.cs
+++ Project Startup/Assets/Scenes/BetaScene/Text/TextCreation.cs
@@ -3,18 +3,30 @@ using UnityEngine.UI;
 
 public class TextCreation : MonoBehaviour {
 
-  private string textInput;
+  [SerializeField] Text textPrefab; // Reference to your Text prefab
+  [SerializeField] Transform textParent; // Parent transform for the instantiated Text objects
+
 
   private void Start() {
-
-  }
-
-  private void Update() {
     
   }
 
+  private void Update() {
+
+  }
+
   public void ReadStringInput(string givenText) {
-    textInput = givenText;
-    Debug.Log("text input is: " + textInput);
+    Debug.Log("text input is: " + givenText);
+    AddTextToPlaceholder(givenText);
+  }
+
+  private void AddTextToPlaceholder(string inputText) {
+    // Instantiate a new Text GameObject from the prefab
+    Text newText = Instantiate(textPrefab, textParent);
+
+    // Set the text value of the instantiated Text
+    newText.text = inputText;
+
+    newText.transform.position = new Vector3(Screen.width / 2, Screen.height / 2, 0.0f);
   }
 }
--- Project Startup/Assets/Scenes/BetaScene/Transform/UITransformPosition.cs
+++ Project Startup/Assets/Scenes/BetaScene/Transform/UITransformPosition.cs
@@ -4,14 +4,6 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class UITransformPosition : MonoBehaviour, IDragHandler {
-  void Start() {
-
-  }
-
-  void Update() {
-
-  }
-
 
   public void OnDrag(PointerEventData eventData) {
     transform.position = Input.mousePosition;
